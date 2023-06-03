ea883be0ac48d72c1013a2259a75f4258ba54398
PSV <pavshin@mail.ru>
2023-06-03 09:16:22 +0700

    Animals added

---
+++ src/java/main/Entities/Camel.java
@@ -0,0 +1,7 @@
+package Entities;
+
+public class Camel extends Pack {
+    public Camel(String species, String trait, String color, String birthdate, String name, String habit) {
+        super(species, trait, color, birthdate, name, habit);
+    }
+}
---
+++ src/java/main/Entities/Humster.java
@@ -0,0 +1,7 @@
+package Entities;
+
+public class Humster extends Pet {
+    public Humster(String species, String trait, String color, String birthdate, String name, String skill) {
+        super(species, trait, color, birthdate, name, skill);
+    }
+}
